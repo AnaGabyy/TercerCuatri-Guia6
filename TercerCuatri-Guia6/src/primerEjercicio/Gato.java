ed8a16824bac68912bfcb652a0d958f4c2f8ac66
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-11 02:30:13 -0300

    Clase Gato (extends Animal)

---
+++ TercerCuatri-Guia6/src/primerEjercicio/Gato.java
@@ -0,0 +1,13 @@
+package primerEjercicio;
+
+public class Gato extends Animal{
+
+    public Gato(String nombre, String sonido) {
+        super(nombre, sonido);
+    }
+
+    @Override
+    public String hacerSonido() {
+        return super.hacerSonido();
+    }
+}
