21e79905ca7837a80f480d06cba79af7297fdcbf
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-11 02:31:05 -0300

    Clase Veterinario
    Métodos: tratarAnimal (recibe por paramétro perro y gato)

---
+++ TercerCuatri-Guia6/src/primerEjercicio/Veterinario.java
@@ -0,0 +1,9 @@
+package primerEjercicio;
+
+public class Veterinario {
+
+    public void tratarAnimal(Perro perro, Gato gato){
+        System.out.println("Animales tratados: " + perro.getNombre() + ", " + gato.getNombre());
+    }
+
+}
