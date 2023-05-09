06094cdc24d9fd18f996b9192c5c3bc1ea5e1f21
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:51:24 -0300

    Clase Profesor
    Hereda de Persona

---
+++ TercerCuatri-Guia6/src/quintoEjercicio/Profesor.java
@@ -0,0 +1,13 @@
+package quintoEjercicio;
+
+public class Profesor extends Persona{
+
+    public Profesor(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void darClase() {
+        System.out.println("El profesor " + getNombre() + " da clases");
+    }
+}
