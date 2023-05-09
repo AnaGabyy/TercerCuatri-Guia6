ae2e0dae8d70ce1017b64beba6bf56e37cc0b26a
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:52:16 -0300

    Clase Curso
    Con metodo inscribrirse qie recibe un objeto tipo Persona

---
+++ TercerCuatri-Guia6/src/quintoEjercicio/Curso.java
@@ -0,0 +1,11 @@
+package quintoEjercicio;
+
+public class Curso {
+
+    public Curso() {
+    }
+
+    public void inscribirse(Persona persona){
+        System.out.println("Está inscrito");
+    }
+}
