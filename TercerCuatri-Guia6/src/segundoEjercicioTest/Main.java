e6ae7c6048c28e8fb5e63f7d9ac044c65686bcaf
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:50:09 -0300

    Clase Main ej2
    Enunciado

--- TercerCuatri-Guia6/src/segundoEjercicioTest/Main.java
+++ TercerCuatri-Guia6/src/segundoEjercicioTest/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*Crea una interfaz llamada Validador con un método llamado validar() y dos subinterfaces llamadas
+        ValidadorDeTexto y ValidadorDeNumero. Crea una clase y agrega un método que tome un objeto de tipo Validador
+        y lo utilice para validar un valor. Luego, crea una clase que implemente las subinterfaces y defina el método
+        validar() para cada tipo de validador. Finalmente, crea un objeto de cada subinterfaz y pásalos al método
+        que toma un Validador.*/
+
     }
 }
