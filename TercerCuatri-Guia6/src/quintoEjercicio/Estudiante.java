beadb072648343a2cc532a549c6b22f1b8a080c1
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:51:42 -0300

    Clase Estudiante
    Hereda de Persona

---
+++ TercerCuatri-Guia6/src/quintoEjercicio/Estudiante.java
@@ -0,0 +1,13 @@
+package quintoEjercicio;
+
+public class Estudiante extends Persona{
+
+    public Estudiante(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void darClase() {
+
+    }
+}
