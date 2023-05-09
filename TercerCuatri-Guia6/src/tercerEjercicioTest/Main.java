48578016a376750e01eee03c14bc51608535f680
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:49:49 -0300

    Clase Main ej3
    Enunciado

--- TercerCuatri-Guia6/src/tercerEjercicioTest/Main.java
+++ TercerCuatri-Guia6/src/tercerEjercicioTest/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*Crea una clase llamada Cajero y dos subclases llamadas CajeroDeBanco y CajeroAutomatico.
+        Agrega un método llamado realizarTransaccion() a la clase Cajero y redefine este método en las subclases
+        para que cada subclase procese la transacción de manera diferente. Luego, crea una instancia de Cajero
+        y llama a su método realizarTransaccion() pasándole tanto un objeto de tipo CajeroDeBanco como uno de
+        tipo CajeroAutomatico.
+         */
+
     }
 }
