f0331842fc39c5e8c323c719b45270f7fbdaf267
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 20:49:27 -0300

    Clase Main ej4
    Enunciado

--- TercerCuatri-Guia6/src/cuartoEjercicioTest/Main.java
+++ TercerCuatri-Guia6/src/cuartoEjercicioTest/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*Crea una interfaz llamada Estrategia con un método llamado ejecutar() y dos subinterfaces llamadas
+        EstrategiaOfensiva y EstrategiaDefensiva. Agrega un método que tome un objeto de tipo Estrategia y
+        lo utilice para ejecutar una acción. Luego, crea una clase que implemente las subinterfaces y defina
+        el método ejecutar() para cada tipo de estrategia. Finalmente, crea un objeto de cada subinterfaz y
+        pásalos al método que toma una Estrategia.*/
+
     }
 }
